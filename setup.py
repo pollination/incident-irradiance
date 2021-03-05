76a8ed0a134bc3913f7870004d463747ec5b3e29
Chris Mackey <chris@ladybug.tools>
2021-03-04 21:24:23 -0500

    fix(deps): Bump dependencies
    I am also cleaning up the documentation a bit

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     setup_requires=['setuptools_scm'],
     url='https://github.com/pollination/annual-radiation',                   # will be translated to home
     project_urls={
-        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/annualrecipe.png',
+        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/annualradrecipe.png',
         'docker': 'https://hub.docker.com/r/ladybugtools/honeybee-radiance'
     },
     description='Annual radiation recipe for Pollination.',                  # will be used as package description
